aa100a81c92053635253159dd5969fc76a0a3d52
ali Hassan <a.hassan13013@outlook.com>
2024-05-28 19:52:19 +0500

    static content

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Events from './pages/Events'
+import Speakers from './pages/Speakers'
+import Testimonials from './pages/Testimonials'
+import UniMemeMingles from './pages/UniMemeMingles'
+import Blog from './pages/Blog'
+import UniStyleEventShop from './pages/UniStyleEventShop'
+import Navbar from './pages/Navbar'
+import Header from './pages/Header'
+import Footer from './pages/Footer'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/events' element={<Events />} />
+        <Route path='/speakers' element={<Speakers />} />
+        <Route path='/testimonials' element={<Testimonials />} />
+        <Route path='/uniMemeMingles' element={<UniMemeMingles />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/uniStyleEventShop' element={<UniStyleEventShop />} />
+      </Routes>
+      <Footer />
     </>
+
   )
 }
 
